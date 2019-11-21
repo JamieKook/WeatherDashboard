0db4c11fd88575a40cfca4550d0cdcf9860cbca1
Jamie <kookjc6@gmail.com>
2019-11-21 14:46:00 -0600

    have city name, icon, temp, and humidity displaying but somethings wrong with temp

---
+++ script.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+    let appID = "2283f8cb8d1d8568c222f4ed3459c3b4";
+    
+    $("#searchbtn").on("click", function(){
+        event.preventDefault(); 
+        let searchCity= $(this).prev().val();
+        
+        let weather= "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&APPID=" + appID;
+       
+        $.getJSON(weather, function(json){
+            $("#currentCity").html(json.name); 
+            $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+            $("#temp").html(json.main.temp);
+            $("#humidity").html(json.main.humidity);
+        })
+        }); 
+
+})
+
+// need to get moment.js to get todays date on display 
