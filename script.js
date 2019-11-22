6943b532d957aaa05b2c53b9bbb5113b8b60fb41
Jamie <kookjc6@gmail.com>
2019-11-22 10:10:01 -0600

    added description to current weather

--- script.js
+++ script.js
@@ -8,15 +8,17 @@ $(document).ready(function(){
         let weather= "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&APPID=" + appID;
         console.log(weather); 
         $.getJSON(weather, function(json){
+            console.log(json); 
             $("#currentCity").html(json.name); 
             $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
             $("#temp").html(((json.main.temp-273.15) * 9/5 + 32).toFixed(1)+"&#8457");
             $("#humidity").html(json.main.humidity+"%");
             $("#windspeed").html(((json.wind.speed)* 2.237).toFixed(1)+" MPH"); 
+            $("#description").html("Description: "+json.weather[0].description); 
         })
         
         let forecast="http://api.openweathermap.org/data/2.5/forecast?q=" + searchCity +"&units=imperial&APPID=" + appID;
-        console.log(forecast); 
+        
         $.ajax({
             url: forecast,
             method: "GET"
@@ -25,13 +27,11 @@ $(document).ready(function(){
             let allForecastDays= response.list; 
             console.log(allForecastDays);
             let dayCount=1; 
-
             for (let i=0; i <allForecastDays.length; i++){
                 let dateFull= allForecastDays[i].dt_txt; 
                 let date=dateFull.split(" ")[0];
                 let time= dateFull.split(" ")[1];  
                 if(time === "15:00:00"){
-                    console.log(time); 
                     let year= date.split("-")[0];
                     let month=date.split("-")[1];
                     let day = date.split("-")[2];
@@ -40,12 +40,12 @@ $(document).ready(function(){
                     $("#day"+dayCount).children(".card-humid").html("Humidity: " +allForecastDays[i].main.humidity+"%"); 
                     $("#day"+dayCount).children(".card-icon").html("<img src=http://openweathermap.org/img/w/" + allForecastDays[i].weather[0].icon + ".png>" ); 
                     dayCount++; 
-            } 
-                }
+                } 
+            }
                 
              
         })
-        }); 
+    }); 
 
 })
 
